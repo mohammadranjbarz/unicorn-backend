9ef2f30ea892ab143cc4e4803db2e43292e74257
Mahdi <mahdighajary@gmail.com>
2024-05-14 17:39:14 +0330

    Add all token pricings endpoint

--- src/pricing/pricing.controller.ts
+++ src/pricing/pricing.controller.ts
@@ -2,6 +2,10 @@ import { Body, Controller, Get, Logger } from '@nestjs/common';
 
 import { PricingService } from './pricing.service';
 import { GetPrice, GetPricesBulk } from './dto/pricing.dto';
+import {
+  frontendSupportedSanityTokenPrices,
+  frontendSupportedTokens,
+} from './types/tokens';
 
 @Controller({ path: 'pricing' })
 export class PricingController {
@@ -19,5 +23,25 @@ export class PricingController {
       tokens.map((item) => this.pricingService.getPrice(item)),
     );
     return prices;
+  }
+
+  // All supported tokens on the client-side
+  @Get('/all')
+  async getAllSupportedTokens() {
+    const prices = await Promise.all(
+      frontendSupportedTokens.map((item) => this.pricingService.getPrice(item)),
+    );
+    const result = prices.reduce(
+      (acc, curr, index) =>
+        curr !== 1
+          ? {
+              ...acc,
+              [frontendSupportedTokens[index]]: curr,
+            }
+          : acc,
+      {},
+    );
+
+    return { ...frontendSupportedSanityTokenPrices, ...result };
   }
 }
