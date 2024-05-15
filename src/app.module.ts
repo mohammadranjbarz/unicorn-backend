825d030d8bac9cba02fc41481f07a581f5c83d43
Mahdi Ghajary <46849131+mmahdigh@users.noreply.github.com>
2024-05-15 14:33:12 +0330

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 // import { UsersModule } from './user/users.module';
-// import { NsModule } from './ns/ns.module';
+import { NsModule } from './ns/ns.module';
+import { PoapModule } from './poap/poap.module';
 import { PricingModule } from './pricing/pricing.module';
 
 @Module({
