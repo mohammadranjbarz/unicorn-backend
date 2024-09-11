bac0e6c253c05eb0f322d3bde61b4700f6ecba5d
Youssef <yussdev@gmail.com>
2024-09-11 15:48:39 +0330

    feat(account): add new fields

---
+++ prisma/migrations/20240911121815_add_new_fields_account/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `verifier` on the `Account` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[verifier_address]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `verifier_address` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Account_verifier_idx";
+
+-- DropIndex
+DROP INDEX "Account_verifier_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "verifier",
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "first_name" TEXT,
+ADD COLUMN     "got_airdropped" BOOLEAN,
+ADD COLUMN     "last_name" TEXT,
+ADD COLUMN     "verifier_address" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_verifier_address_key" ON "Account"("verifier_address");
+
+-- CreateIndex
+CREATE INDEX "Account_verifier_address_idx" ON "Account"("verifier_address");
