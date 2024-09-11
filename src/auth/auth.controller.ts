00046965f0b3d40b75feee7b8d5ecf666894f7cf
Youssef <yussdev@gmail.com>
2024-09-11 11:26:44 +0330

    chore: force deploy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -28,7 +28,7 @@ export class AuthController {
   })
   @Get('/isLoggedIn')
   async isLoggedIn(@Req() req: AuthedReq) {
-    return req.userId || 'foo';
+    return req.userId || 'no-user';
   }
 
   @Post('/logout')
